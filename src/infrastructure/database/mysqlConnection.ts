cade7f5aef57e2db96f2e7b20e2966011112d2ee
josueCastillejos <84791173+211234@users.noreply.github.com>
2024-07-04 12:28:22 -0600

    Update mysqlConnection.ts
    userMySql

--- src/infrastructure/database/mysqlConnection.ts
+++ src/infrastructure/database/mysqlConnection.ts
@@ -5,7 +5,7 @@ const connectMySQL = async (): Promise<Connection> => {
     try {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
-            user: process.env.DB_Alumno,
+            user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME
         });
